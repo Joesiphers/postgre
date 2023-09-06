91f065012bda39a9019c7a52f6bdd6abce6c1a47
wenguang zhou <joesiphers@163.com>
2023-09-06 14:55:22 +1000

    remove users

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const usersRouter = require("./routes/users");
+//const usersRouter = require("./routes/users");
 const app = express();
 const port = 3000;
 const { Client } = require("pg");
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.use("/users", usersRouter);
+//app.use("/users", usersRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
